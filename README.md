0da9bd46fdcf6efa02ab0e883504d777c8a8fa83
alanpilloud <alanpilloud@gmail.com>
2017-02-01 22:49:43 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# sphinge-interceptor-wordpress
-Intercepts errors and sends them to your Sphinge dashboard
+# Sphinge Interceptor for WordPress
+
+This MU plugin intercepts errors and sends them to your Sphinge dashboard.
+
+## Requirements
+
+You need to have a Sphinge dashboard installed and the Sphinge for WordPress installed and configured on your website.
+
+## Installation
+
+Download the package and upload the file `sphinge-interceptor.php` in your `/wp-content/mu-plugins` folder. If this folder doesn't already exists, you can create it.
---
+++ sphinge-interceptor.php
@@ -0,0 +1,93 @@
+<?php
+/*
+Plugin Name: Sphinge Interceptor
+Plugin URI: https://sphinge.io/extensions/interceptor
+Description: Intercepts errors and sends them to your Sphinge dashboard.
+Version: 1.0.0-beta
+Author: Sphinge
+Author URI: https://sphinge.io
+*/
+
+// get sphinge config file
+$config_file_exists = include ABSPATH.'sphinge'.DIRECTORY_SEPARATOR.'config.php';
+
+if (!$config_file_exists) {
+    add_action('admin_notices', function () {
+        echo '<div class="notice notice-error"><b>Sphinge Interceptor &mdash; </b>'.__('You must install Sphinge for WordPress first and provide your dashboard URL.', 'sphinge').'</div>';
+    });
+} elseif (!defined('SPHINGE_URL')) {
+    add_action('admin_notices', function () {
+        echo '<div class="notice notice-error"><b>Sphinge Interceptor &mdash; </b>'.__('You must provide your dashboard URL.', 'sphinge').'</div>';
+    });
+} else {
+    /**
+     * Everything's fine, let's go and set error handlers
+     */
+    set_error_handler('intercept_error');
+    register_shutdown_function('intercept_fatal_error');
+    set_exception_handler('intercept_uncaught_exception');
+}
+
+/**
+ * Intercepts errors and sends them
+ *
+ * @param  [type] $errno   [description]
+ * @param  [type] $errstr  [description]
+ * @param  [type] $errfile [description]
+ * @param  [type] $errline [description]
+ *
+ * @return void
+ */
+function intercept_error($errno, $errstr, $errfile, $errline) {
+    sendError([
+        'type' => $errno,
+        'message' => $errstr,
+        'file' => $errfile,
+        'line' => $errline
+    ]);
+}
+
+/**
+ * Intercepts fatal errors that are not managed by intercept_error and sends them
+ *
+ * @return void
+ */
+function intercept_fatal_error() {
+    $lastError = error_get_last();
+
+    if (in_array($lastError['type'], [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR, E_USER_ERROR])) {
+        sendError([
+            'type' => $lastError['type'],
+            'message' => $lastError['message'],
+            'file' => $lastError['file'],
+            'line' => $lastError['line']
+        ]);
+    }
+}
+
+/**
+ * Intercepts uncaught exceptions and sends them
+ *
+ * @param  Exception $exception Intercepted exception
+ *
+ * @return void
+ */
+function intercept_uncaught_exception($exception) {
+    sendError([
+        'type' => 'Uncaught Exception',
+        'message' => $exception->getMessage(),
+        'file' => $exception->getFile(),
+        'line' => $exception->getLine()
+    ]);
+}
+
+/**
+ * Sends an error to Sphinge
+ *
+ * @param  array  $error The error that will be sent
+ *
+ * @return void
+ */
+function sendError(array $error) {
+    $response = wp_remote_post(SPHINGE_URL.'/api/intercept', ['body' => json_encode($error)]);
+}
